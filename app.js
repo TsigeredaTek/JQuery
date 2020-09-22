f37304c511724d4008d9226a2e5423f0d20cb807
TsigeredaTek <rose_ym@yahoo.com>
2020-09-22 14:55:07 -0500

    Done

---
+++ app.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $('input[type=text]').keyup(function () {
+        if ($(this).val() == "") {
+            $('#btnSubmit').prop('disabled', true);
+        } else {
+            $('#btnSubmit').prop('disabled', false);
+        };
+    });
+    $('body').append('<ul></ul>');
+
+    $('#btnSubmit').click(function(e){
+        e.preventDefault();
+        let text = $('input[type=text]').val();
+        $('ul').append(`<li>${text}</li>`);
+    });
+
+    $('ul').click(function(event){
+        $(event.target).css({'color':getRandomColor()});    
+    });
+
+    // get random color for li
+    function getRandomColor() {
+        let randomColor = 'rgb(' + Math.floor((Math.random() * 256)) + ',' + Math.floor((Math.random() * 256)) +',' + Math.floor((Math.random() * 256)) + ')';
+        return randomColor;
+    };
+
+    //remove li after it is double clicked
+    $('ul').dblclick(function(event){
+        $(event.target).remove();
+    });
+});
